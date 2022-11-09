d51a2e50ea5e43bf6afba695d1198eed25d697cf
DOER <wangrui20211@126.com>
2022-11-09 20:40:00 +0800

    Leraning for git commit,nothing important changed.

--- src/bookcontent/basic01/Example6_7.java
+++ src/bookcontent/basic01/Example6_7.java
@@ -9,13 +9,13 @@ class AdvertisementBoard { //负责创建广告牌
     Advertisement adver;
     public void setAdvertisement(Advertisement adver){
         this.adver = adver;
-    }
+    }//接口类型参数adver
     public void show() {
         if(adver == null){
             System.out.println("广告招商中");
         }
         else {
-            adver.showAdvertisement(); //接口回调
+            adver.showAdvertisement(); //接口回调，调用showAdvertisement方法
         }
     }
 }
